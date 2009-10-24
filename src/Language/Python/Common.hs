75cef20865d4dede87bcbddd14e9277ef4a1bcda
bjpop <bjpop@csse.unimelb.edu.au>
2009-10-24 23:31:34 +1100

    add Common.hs file
    darcs-hash:20091024123134-639e1-bc86bc9513fdc31d5ae07b3b55e9274282a449eb.gz

---
+++ src/Language/Python/Common.hs
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Language.Python.Common
+-- Copyright   : (c) 2009 Bernie Pope 
+-- License     : BSD-style
+-- Maintainer  : bjpop@csse.unimelb.edu.au
+-- Stability   : experimental
+-- Portability : ghc
+--
+-----------------------------------------------------------------------------
+
+module Language.Python.Common
+   (module PrettyClass, module Token) where
+
+import Language.Python.Common.Token as Token 
+import Language.Python.Common.PrettyClass as PrettyClass 
