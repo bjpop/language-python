ffb0158f30c01d0094282442948bdec98d1e7153
bjpop <bjpop@csse.unimelb.edu.au>
2009-10-29 17:40:24 +1100

    Added missing Version2.hs file
    darcs-hash:20091029064024-639e1-667db88436c1bc943b8e75d11129466aa8d2601a.gz

---
+++ src/Language/Python/Version2.hs
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Language.Python.Version2
+-- Copyright   : (c) 2009 Bernie Pope 
+-- License     : BSD-style
+-- Maintainer  : bjpop@csse.unimelb.edu.au
+-- Stability   : experimental
+-- Portability : ghc
+--
+-- A wrapper module for the python version 2.x parser/lexer. 
+--
+-- See: 
+--
+-- * <http://www.python.org/doc/2.6/reference/index.html> for an overview of the language. 
+--
+-- * <http://www.python.org/doc/2.6/reference/grammar.html> for the full grammar.
+-- 
+-- * <http://www.python.org/doc/2.6/reference/toplevel_components.html> for a description of 
+-- the various Python top-levels, which correspond to the parsers provided here.
+-----------------------------------------------------------------------------
+
+module Language.Python.Version2 
+   (module Parser, module Lexer, module Pretty) where
+
+import Language.Python.Version2.Parser as Parser 
+import Language.Python.Version2.Lexer as Lexer hiding (ParseError)
+import Language.Python.Version2.Syntax.Pretty as Pretty
